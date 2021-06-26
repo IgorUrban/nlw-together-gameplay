8910513bd2e292a18c79a5b1c7dcd5ef256c9146
Igor Urban <igurbanca@gmail.com>
2021-06-26 20:59:35 -0300

    Criando tela de agendamento de partidas

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -3,7 +3,7 @@ import { Image } from "react-native";
 import { styles } from "./styles";
 
 export function GuildIcon() {
-  const uri = 'https://github.com/IgorUrban.png';
+  const uri = 'https://w7.pngwing.com/pngs/942/98/png-transparent-discord-computer-icons-teamspeak-computer-servers-others-miscellaneous-blue-smiley.png';
 
   return (
     <Image 
