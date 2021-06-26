da4b1774e02d5a9793cb0afc35d6f517658f1763
Igor Urban <igurbanca@gmail.com>
2021-06-26 17:10:05 -0300

    Criando listagem de servidores na página Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,30 +1,75 @@
-import React, {useState} from 'react';
-import { View } from 'react-native'
+import React, { useState } from "react";
+import { View, FlatList } from "react-native";
 
-import { CategorySelect } from '../../components/CategorySelect';
-import { ButtonAdd } from '../../components/ButtonAdd';
-import { Profile } from '../../components/Profile';
+import { CategorySelect } from "../../components/CategorySelect";
+import { ButtonAdd } from "../../components/ButtonAdd";
+import { Profile } from "../../components/Profile";
+import { ListHeader } from "../../components/ListHeader";
+import { Appointment } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
 
-import { styles } from './styles'
+import { styles } from "./styles";
 
-export function Home(){
-    const [category, setCategory] = useState('');
+export function Home() {
+  const [category, setCategory] = useState("");
 
-    function handleCategorySelect(categoryId: string){
-        categoryId === category ? setCategory('') : setCategory(categoryId);
-    }
+  const appointments = [
+    {
+      id: "1",
+      guild: {
+        id: "1",
+        name: "Ledários",
+        icon: null,
+        owner: true,
+      },
+      category: "1",
+      date: "22/06 às 20:40h",
+      description: "É hoje que vamos chegar ao challenger",
+    },
+    {
+      id: "2",
+      guild: {
+        id: "1",
+        name: "Ledários",
+        icon: null,
+        owner: true,
+      },
+      category: "1",
+      date: "22/06 às 20:40h",
+      description: "É hoje que vamos chegar ao challenger",
+    },
+  ];
 
-    return(
-        <View>
-            <View style={styles.header}>
-                <Profile />
-                <ButtonAdd/>
-            </View>
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory("") : setCategory(categoryId);
+  }
 
-            <CategorySelect 
-                categorySelected={category} 
-                setCategory = {handleCategorySelect}
-            />
+  return (
+    <View>
+      <View style={styles.header}>
+        <Profile />
+        <ButtonAdd />
+      </View>
+
+      <View>
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect}
+        />
+
+        <View style={styles.content}>
+          <ListHeader title="Partidas agendadas" subtitle="Total 6" />
+
+          <FlatList
+            data={appointments}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <Appointment data={item} />}
+            style={styles.matches}
+            showsVerticalScrollIndicator={false}
+            ItemSeparatorComponent={() => <ListDivider />}
+          />
         </View>
-    );
+      </View>
+    </View>
+  );
 }
